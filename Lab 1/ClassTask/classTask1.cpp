c1e00cf80e3b8bb2c8845c31649070a6975be81c
Syeda Marium Fatima <syedamariumfatima20@gmail.com>
2026-08-18 22:44:52 +0500

    Update classTask1.cpp

--- Lab 1/ClassTask/classTask1.cpp
+++ Lab 1/ClassTask/classTask1.cpp
@@ -1,1 +1,27 @@
+#include <iostream>
+using namespace std;
 
+int main() 
+{
+    int* arr;
+    int n;
+    cout << "Enter the number of elements";
+    arr=new int[n];
+    for(int i=0; i<n; i++){
+        cout<<"enter element"<<i;
+        cin>>*(arr+i);
+    }
+    int max=arr[0], min=arr[0], sum=arr[0];
+    for(int i=1; i<n; i++){
+        if(arr[i]>max) max=arr[i];
+        else if(arr[i]<min) min=arr[i];
+        sum+=arr[i];
+    }
+    int avg=sum/n;
+
+    cout<<"sum:"<<sum;
+    cout<<"average:"<<avg;
+    cout<<"Maximum:"<<max;
+    cout<<"Minimum"<<min;
+    return 0;
+}
