60c5c6681966a65f874625f9cabfaccdf4e79a52
Syeda Marium Fatima <syedamariumfatima20@gmail.com>
2026-08-19 12:04:34 +0500

    Implement rectangle class with area and display methods
    Added a rectangle class with area calculation and display functionality.

--- Lab 1/ClassTask/classTask2.cpp
+++ Lab 1/ClassTask/classTask2.cpp
@@ -1,1 +1,31 @@
+#include <iostream>
+using namespace std;
 
+class rectangle{
+    int* width;
+    int* height;
+    public:
+    rectangle(int w, int h){
+        width=new int(w);
+        height=new int(h);
+    }
+    int area(){
+        return (*width)*(*height);
+    }
+    void display(){
+        cout<<"width: "<<*width<<endl;
+        cout<<"height: "<<*height<<endl;
+        cout<<"area: "<<area()<<endl;
+    }
+    ~rectangle(){
+        delete[] width;
+        delete[] height;
+        cout<<"rectangle destroyed";
+    }
+};
+int main() 
+{
+    rectangle r(3,5);
+    r.display();
+    return 0;
+}
