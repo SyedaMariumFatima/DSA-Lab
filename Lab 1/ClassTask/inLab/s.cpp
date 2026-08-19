6b03130c2305ad6cfc05a41f2595b477d5b6c619
Syeda Marium Fatima <syedamariumfatima20@gmail.com>
2026-08-19 12:36:56 +0500

    Add student class with input and display methods
    Implement a student class with methods for input, display, and average calculation.

---
+++ Lab 1/ClassTask/inLab/s.cpp
@@ -0,0 +1,53 @@
+#include<iostream>
+using namespace std;
+
+class student{
+	public:
+		int* arr;
+		int size;
+		int t=0;
+	student(int s){
+		size=s;
+		arr=new int[size];
+		for(int i=0; i<size; i++){
+			cin>>*(arr+i);
+		}
+	}
+	student(student &other){
+		size=other.size;
+		for(int i=0; i<size; i++){
+			arr[i]=other.arr[i];
+		}
+	}
+	void input(){
+			for(int i=0; i<size; i++){
+			cin>>*(arr+i);
+		}
+	}
+	void display(){
+		for(int i=0; i<size; i++){
+			cout<<*(arr+i)<<" ";
+		}
+		cout<<endl;
+	}
+	void avg(){
+		for(int i=0; i<size; i++){
+			t+= *(arr+i);
+		}
+		cout<<t/size;
+	}
+	
+	~student(){
+		delete[] arr;
+		cout<<endl<<"destructor called";
+	}
+};
+
+int main(){
+	student s(5);
+	student s2(s);
+	s2.input();
+	s.display();
+	s2.display();
+	return 0;
+}
